e42a38fb23d5ca5a738fc564827cab3ee9b14e63
Jin-Woo Kim <dnehd6739@naver.com>
2022-01-03 21:17:08 +0900

    bellman-ford

--- 11657.py
+++ 11657.py
@@ -1,4 +1,4 @@
-# 문제 : 타임머신
+# 문제 : 타임머신(벨만 포드)
 # N개의 도시가 있다. 그리고 한 도시에서 출발하여 다른 도시에 도착하는 버스가 M개 있다.
 # 각 버스는 A, B, C로 나타낼 수 있는데, A는 시작도시, B는 도착도시, C는 버스를 타고 이동하는데 걸리는 시간이다.
 # 시간 C가 양수가 아닌 경우가 있다. C = 0인 경우는 순간 이동을 하는 경우,
@@ -9,5 +9,37 @@
 # 둘째 줄부터 M개의 줄에는 버스 노선의 정보 A, B, C (1 ≤ A, B ≤ N, -10,000 ≤ C ≤ 10,000)가 주어진다.
 # 출력
 # 만약 1번 도시에서 출발해 어떤 도시로 가는 과정에서 시간을 무한히 오래 전으로 되돌릴 수 있다면 첫째 줄에 -1을 출력한다. 그렇지 않다면 N-1개 줄에 걸쳐 각 줄에 1번 도시에서 출발해 2번 도시, 3번 도시, ..., N번 도시로 가는 가장 빠른 시간을 순서대로 출력한다. 만약 해당 도시로 가는 경로가 없다면 대신 -1을 출력한다
+import sys
 
+N,M = map(int,sys.stdin.readline().split()) # 도시 개수, 버스 노선 개수
+graph = []
+dist = [1e9]*(N+1)
+for _ in range(M):
+    u,v,w = map(int,sys.stdin.readline().split())
+    graph.append((u,v,w))
+
+def bellman_ford(start):
+    dist[start] = 0
+    for i in range(N):
+        for j in range(M):
+            node = graph[j][0]
+            next_node = graph[j][1]
+            weight = graph[j][2]
+            if dist[node] != 1e9 and dist[next_node] > dist[node] + weight:
+                dist[next_node] = dist[node] + weight
+                if i == N-1:
+                    return True
+    return False
+
+
+negative = bellman_ford(1)
+
+if negative:
+    print(-1)
+else:
+    for i in range(2,N+1):
+        if dist[i] == 1e9:
+            print(-1)
+        else:
+            print(dist[i])
 
